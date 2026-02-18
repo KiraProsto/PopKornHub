9d4c95b2d784fe59962dcdac5dc619ff6f732d9a
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-02-18 14:28:11 +0300

    Display TMDB movies on Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,53 @@
+import  {useEffect, useState } from 'react';
+import { getMoviesByCategory } from '../../api/movies';
+
 export default function Home(){
+    const [popular, setPopular] =useState([]);
+    const [topRated, setTopRated] = useState([]);
+    const [upcoming, setUpcoming] = useState([]);
+
+    useEffect(() =>{
+        async function loadMovies() {
+            const popularMovies = await getMoviesByCategory('popular');
+            const topRatedMovies = await getMoviesByCategory('top_rated');
+            const upcomingMovies = await getMoviesByCategory('upcoming');
+
+            setPopular(popularMovies);
+            setTopRated(topRatedMovies);
+            setUpcoming(upcomingMovies);
+        }
+
+        loadMovies();
+    }, []); 
+
     return(
         <div>
-            Home page
+            <h1>Популярные</h1>
+            <ul>
+                {popular.map((movie: any) => (
+                    <li key = {movie.id}>
+                        {movie.title}
+                    </li>
+                ))}
+            </ul>
+
+            <h1>Топ рейтинга</h1>
+            <ul>
+                {topRated.map((movie: any) => (
+                    <li key = {movie.id}>
+                        {movie.title}
+                    </li>
+                ))}
+            </ul>
+
+            <h1>Скоро в эфире!</h1>
+            <ul>
+                {upcoming.map((movie: any) => (
+                    <li key = {movie.id}>
+                        {movie.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
