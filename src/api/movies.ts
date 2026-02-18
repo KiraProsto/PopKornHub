7165e93018bea611179333224f198a0c5d81998f
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-02-18 13:52:56 +0300

    Add TMDB API integration

---
+++ src/api/movies.ts
@@ -0,0 +1,16 @@
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export async function getMoviesByCategory(category: string) {
+    const url = `${BASE_URL}/movie/${category}?api_key=${API_KEY}&language=ru-RU`;    
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+}   
+
+export async function getMovieDetails(id:string) {
+    const url = `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=ru-RU`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;    
+}
