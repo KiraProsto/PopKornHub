67faae2bc0c4ca7d150dc514bcc74f484e3963a6
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-02-19 15:09:30 +0300

    Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import {Link} from "react-router-dom";
+import styles from './Header.module.scss'
+
+export default function Header(){
+    return(
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src="/popkorn.svg" alt="PopKornHub" className={styles.image} />
+                <h1 className={styles.title}>
+                    PopKorn
+                </h1>
+            </div>
+
+            <nav className={styles.nav}>
+                <Link to="/" className={styles.link}>
+                    Home
+                </Link>
+                <Link to="/favorites" className={styles.link}>
+                    Favorites
+                </Link>
+            </nav>
+        </header>
+    )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,12 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from "../pages/Home/Home";
 import MovieDetails from '../pages/MovieDetails/MovieDetails';
 import Favorites from '../pages/Favorites/Favorites';
+import Header from '../components/Header/Header';
 
 export default function Router() {
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path = '/' element = {<Home />}/>
                 <Route path = '/movie/:id' element = {<MovieDetails />}/>
