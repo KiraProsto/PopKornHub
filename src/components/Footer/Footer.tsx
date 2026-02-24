313a89d247572f6683441723caeb048c5212883c
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-02-24 11:59:12 +0300

    fix(year): make footer year dynamic

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import "./Footer.scss"
 
 export default function Footer(){
+    const currentYear = new Date().getFullYear();
+
     return(
         <footer className="footer" aria-label="Подвал сайта">
             <p className="footer__text">
-                © 2026 PopKornHub - made with 🍿 by Kira
+                © {currentYear} PopKornHub - made with 🍿 by Kira
             </p>
         </footer>
     );
